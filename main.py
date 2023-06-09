0b38b0d93e5acac2976b03240f0798de548a79b9
itokiana-mahefarison <81515527+itokiana-mahefarison@users.noreply.github.com>
2023-06-09 12:07:21 +0000

    final modif

--- main.py
+++ main.py
@@ -3,27 +3,36 @@ import os
 from PIL import Image
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import accuracy_score
 
-input_dir = "fer2013"
+train_input_dir = "fer2013/train"
+test_input_dir = "fer2013/test"
 categories = [
   "angry", "disgust", "fear", "happy", "neutral", "sad", "surprise"
 ]
 
-datas = []
-target = []
 
-target_size = (48, 48)
+def numeric_images(input_dir, categories):
+  datas = []
+  target = []
+  target_size = (48, 48)
 
-for index, category in enumerate(categories):
-  for file in os.listdir(os.path.join(input_dir, category)):
-    img_path = os.path.join(input_dir, category, file)
-    image = Image.open(img_path).convert("L")
-    image = image.resize(target_size)
-    datas.append(list(image.getdata()))
-    target.append(index)
+  for index, category in enumerate(categories):
+    for file in os.listdir(os.path.join(input_dir, category)):
+      img_path = os.path.join(input_dir, category, file)
+      image = Image.open(img_path).convert("L")
+      image = image.resize(target_size)
+      datas.append(list(image.getdata()))
+      target.append(index)
 
-datas = np.asarray(datas)
-target = np.asarray(target)
+  datas = np.asarray(datas)
+  target = np.asarray(target)
+
+  return (datas, target)
+
+
+x_train, y_train = numeric_images(train_input_dir, categories)
+x_test, y_test = numeric_images(test_input_dir, categories)
 
 model = RandomForestClassifier()
 parameters = [{
@@ -36,6 +45,12 @@ parameters = [{
 
 grid_search = GridSearchCV(model, parameters)
 
-grid_search.fit(datas, target)
+grid_search.fit(x_train, y_train)
 
 best_estimator = grid_search.best_estimator_
+
+y_predicted = best_estimator.predict(x_test)
+
+score = accuracy_score(y_test, y_predicted)
+
+print('Score of model: {}'.format(str(score)))
